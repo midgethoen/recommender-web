ae32da61bd4b034fc5464bffdba8c74dcec0b5c1
JasonT <jasonteunissen@gmail.com>
2017-12-01 21:14:15 +0100

    menu lightbox fix
    menu lightbox fix

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Menu.css';
+import './Menu.global.scss';
 import image from './img/ICN32_hamburger@2x.png';
 
 
@@ -21,24 +21,32 @@ export default class Menu extends Component {
     return (
 
 
-      <div
-        className={`menu-wrap ${menuClass}`}
-        onClick={this.toggleMenu}
-      >
-        <div className="menu">
-          <div
-            className="menu-btn"
-            onClick={this.toggleMenu}
-          ><img src={image} /></div>
-          <ul>
-            <li><a href="">Watch</a></li>
-            <li><a href="">Done</a></li>
-            <li><a href="">Outbox</a></li>
-            <li><a href="">Inbox</a></li>
-            <li><a href="">Settings</a></li>
-          </ul>
+      <div className="menu-lightboxbg-wrap">
+        <div
+          className={`menu-wrap ${menuClass}`}
+          onClick={this.toggleMenu}
+        >
+          <div className="menu">
+            <div
+              className="menu-btn"
+              onClick={this.toggleMenu}
+            ><img src={image} /></div>
+            <ul>
+              <li><a href="">Watch</a></li>
+              <li><a href="">Done</a></li>
+              <li><a href="">Outbox</a></li>
+              <li><a href="">Inbox</a></li>
+              <li><a href="">Settings</a></li>
+            </ul>
+          </div>
         </div>
+        {
+          // end menu-lightboxbg
+          menuOpen && <div className="menu-lightboxbg"/>
+        }
       </div>
+
+
 
 
 
